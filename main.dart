03babe48b4a77129a30ad05306c6e13edb332ec2
sobin1796 <sobinr019378@nec.edu.np>
2024-09-04 21:28:49 +0545

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:morph/auth/splashscreen.dart';
+import 'package:morph/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demoo',
+      home: SplashScreen(),
+    );
+  }
+}
